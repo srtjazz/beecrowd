61827eb2a72861329cf59e1b76f03afa21b371fc
Jess Bittencourt <kevinbittencourt87@gmail.com>
2024-04-24 11:55:14 -0300

    exercise code 1143

--- C#/Iniciante/Pagina 3/1143.cs
+++ C#/Iniciante/Pagina 3/1143.cs
@@ -1,5 +1,16 @@
 using System;
 
+//Quadrado e ao Cubo
+
+//Escreva um programa que leia um valor inteiro N (1 < N < 1000). Este N é a quantidade de linhas de saída que
+//serão apresentadas na execução do programa.
+
+//Entrada
+//O arquivo de entrada contém um número inteiro positivo N.
+
+//Saída
+//Imprima a saída conforme o exemplo fornecido.
+
 class URI
 {
 
