cae250a4a9c5e2b03836172742ded868ed4969fc
Jess Bittencourt <kevinbittencourt87@gmail.com>
2024-04-24 11:54:50 -0300

    exercise code 1142

--- C#/Iniciante/Pagina 3/1142.cs
+++ C#/Iniciante/Pagina 3/1142.cs
@@ -1,5 +1,16 @@
 using System;
 
+//PUM
+
+//Escreva um programa que leia um valor inteiro N. Este N é a quantidade de linhas de saída que serão apresentadas
+//na execução do programa.
+
+//Entrada
+//O arquivo de entrada contém um número inteiro positivo N.
+
+//Saída
+//Imprima a saída conforme o exemplo fornecido.
+
 class URI
 {
 
