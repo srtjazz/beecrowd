9fcdd0928e1aa93bd82638ae9ec4216a6a449849
Jess Bittencourt <kevinbittencourt87@gmail.com>
2024-04-25 15:25:42 -0300

    exercise code 1145

--- C#/Iniciante/Pagina 4/1145.cs
+++ C#/Iniciante/Pagina 4/1145.cs
@@ -1,5 +1,17 @@
 using System;
 
+//Sequência Lógica 2
+
+//Escreva um programa que leia dois valores X e Y. A seguir, mostre uma sequência de 1 até Y, passando para a
+//próxima linha a cada X números.
+
+//Entrada
+//O arquivo de entrada contém dois valores inteiros, (1 < X < 20) e (X < Y < 100000).
+
+//Saída
+//Cada sequência deve ser impressa em uma linha apenas, com 1 espaço em branco entre cada número, conforme
+//exemplo abaixo. Não deve haver espaço em branco após o último valor da linha.
+
 class URI
 {
 
