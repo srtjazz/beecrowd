acabc881689d8cf25076643a3b075910454aff71
Jess Bittencourt <kevinbittencourt87@gmail.com>
2024-05-20 18:24:45 -0300

    exercise code 1164

---
+++ C#/Iniciante/Pagina 4/1164.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+//Número Perfeito
+
+//Na matemática, um número perfeito é um número inteiro para o qual a soma de todos os seus divisores positivos próprios
+//(excluindo ele mesmo) é igual ao próprio número. Por exemplo o número 6 é perfeito, pois 1+2+3 é igual a 6. Sua tarefa
+//é escrever um programa que imprima se um determinado número é perfeito ou não.
+
+//Entrada
+//A entrada contém vários casos de teste. A primeira linha da entrada contém um inteiro N (1 ≤ N ≤ 20), indicando o
+//de casos de teste da entrada. Cada uma das N linhas seguintes contém um valor inteiro X (1 ≤ X ≤ 108), que pode ser
+//ou não, um número perfeito.
+
+//Saída
+//Para cada caso de teste de entrada, imprima a mensagem “X eh perfeito” ou “X nao eh perfeito”, de acordo com a especificação fornecida.
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+        {
+            int result = 0;
+            int x = int.Parse(Console.ReadLine());
+            for (int j = 1; j < x; j++)
+            {
+                if (x % j == 0)
+                {
+                    result += j;
+                }
+            }
+            if (result == x)
+            {
+                Console.WriteLine($"{x} eh perfeito");
+            }
+            else
+            {
+                Console.WriteLine($"{x} nao eh perfeito");
+            }
+        }
+    }
+
+}
