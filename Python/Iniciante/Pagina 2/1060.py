fac1888d33f45f4efd05f24ab3fadbbbf9c626a1
Jess Bittencourt <kevinbittencourt87@gmail.com>
2024-07-15 18:05:58 -0300

    exercise code 1060

--- Python/Iniciante/Pagina 2/1060.py
+++ Python/Iniciante/Pagina 2/1060.py
@@ -8,13 +8,11 @@
 
 # Saída
 # Imprima uma mensagem dizendo quantos valores positivos foram lidos.
-counter = 0
 positive_numbers = []
 
-while counter < 6:
+for _ in range(6):
   number_input = float(input())
   if number_input > 0:
     positive_numbers.append(number_input)
-  counter += 1
 
 print(f"{len(positive_numbers)} valores positivos")
