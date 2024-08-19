6536543ad52254ad82dcc71209f245710c5eb024
Jess Bittencourt <kevinbittencourt87@gmail.com>
2024-08-19 14:35:13 -0300

    exercise code 1067

---
+++ Python/Iniciante/Pagina 2/1067.py
@@ -0,0 +1,17 @@
+# Números Ímpares
+
+# Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor
+# por linha, inclusive o X, se for o caso.
+
+# Entrada
+# O arquivo de entrada contém 1 valor inteiro qualquer.
+
+# Saída
+# Imprima todos os valores ímpares de 1 até X, inclusive X, se for o caso.
+
+x = int(input())
+i = 1
+
+for i in range(i, x+1):
+  if i % 2 != 0:
+    print(i)
